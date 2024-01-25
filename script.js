48d0b1bdf1a9111eacb568ec981c98da266bdee4
dirtroaddev <rongoodwindev@gmail.com>
2024-01-25 13:45:59 -0500

    Finished building it out for the students. Will add comments, and better structure soon.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myLibrary = [
-    new Book('Lord of the rings', 'j.r.r.r. tolkien', 256, 'not read'),
+    
 ];
 
 const addBook = document.querySelector('#addBookBtn');
-
+const ul = document.querySelector('#ulDisp');
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -22,9 +22,7 @@ function addBookToLibrary(array, title, author, pages, read) {
 }
 
 function postToPage(arr) {
-    
-    const ul = document.querySelector('#ulDisp');
-
+    ul.innerHtml = ''
     for(let i = 0; i < arr.length; i += 1) {
         const li = document.createElement('li');
         li.textContent = arr[i].info();
@@ -34,12 +32,13 @@ function postToPage(arr) {
 const smtBtn = document.querySelector('#submitBtn');
 
 smtBtn.addEventListener('submit', (e) => {
+
     e.preventDefault();
-    
-    
+    ul.innerHTML = ''
     addBookToLibrary(myLibrary, e.target.elements['title'].value, e.target.elements['author'].value, e.target.elements['pages'].value, e.target.elements['read'].value);
     postToPage(myLibrary);
 });
+
 
 
 
