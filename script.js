95882ed09f4eed9d687f8483121020ce66ef730e
dirtroaddev <rongoodwindev@gmail.com>
2024-01-25 13:39:41 -0500

    Using vanilla JS to teach students how to grab form data

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const myLibrary = [];
+const myLibrary = [
+    new Book('Lord of the rings', 'j.r.r.r. tolkien', 256, 'not read'),
+];
 
+const addBook = document.querySelector('#addBookBtn');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -13,10 +16,31 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibrary() {
+function addBookToLibrary(array, title, author, pages, read) {
+    array.push(new Book(title, author, pages, read));
 
 }
 
-function postToPage() {
+function postToPage(arr) {
     
+    const ul = document.querySelector('#ulDisp');
+
+    for(let i = 0; i < arr.length; i += 1) {
+        const li = document.createElement('li');
+        li.textContent = arr[i].info();
+        ul.appendChild(li);
+    }
 }
+const smtBtn = document.querySelector('#submitBtn');
+
+smtBtn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    
+    addBookToLibrary(myLibrary, e.target.elements['title'].value, e.target.elements['author'].value, e.target.elements['pages'].value, e.target.elements['read'].value);
+    postToPage(myLibrary);
+});
+
+
+
+
